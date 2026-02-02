a0892a14ea789f1a821e13418399d9bfbcc9ba8b
Yinong Kuang <kyn100@gmail.com>
2026-02-01 23:47:49 -0500

    Fix yahoo-finance2 import syntax

--- api/lib/yahooFinance.js
+++ api/lib/yahooFinance.js
@@ -1,7 +1,5 @@
-import YahooFinance from 'yahoo-finance2';
+import yahooFinance from 'yahoo-finance2';
 import axios from 'axios';
-
-const yahooFinance = new YahooFinance();
 
 const FINNHUB_BASE = 'https://finnhub.io/api/v1';
 
